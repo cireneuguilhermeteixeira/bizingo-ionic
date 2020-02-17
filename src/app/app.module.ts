70e34fbb58993fc172c672182e6156454fbe48d8
Cireneu Guilherme Teixeira de Araújo <cireneu.guilherme.teixeira@gmail.com>
2020-02-17 12:21:47 -0300

    corrigindo a porta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,9 @@ import { Settings, User, Api } from '../providers';
 import { MyApp } from './app.component';
 import {DragulaModule} from 'ng2-dragula';
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'http://bizingo-node-server.herokuapp.com', options: {} };
+
+//const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
  
 
 
